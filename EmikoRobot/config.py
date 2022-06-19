1645065017f31549547d250ff8bcec98e42b6c3b
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2022-06-19 21:53:30 +0700

    hmm

--- EmikoRobot/config.py
+++ EmikoRobot/config.py
@@ -18,17 +18,24 @@ class Config(object):
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1606221784  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "excrybaby"
-    SUPPORT_CHAT = "emikosupport"  # Your own group for support, do not add the @
+    OPENWEATHERMAP_ID = 22322
+    OWNER_USERNAME = "Scotchers"
+    BOT_USERNAME = "EmiexRobot"
+    SUPPORT_CHAT = "FumikaSupportGroup"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001432609692
+        -1001748076180
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001150905176
+        -1001748076180
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    ERROR_LOGS = -1001657496255
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
+    MONGO_DB_URI = "mongo+srv"  # needed for any database modules
+    ARQ_API_URL = "https://arq.hamker.in"
+    ARQ_API_KEY = "BCYKVF-KYQWFM-JCMORU-RZWOFQ-ARQ"
+    BOT_API_URL = "https://api.telegram.org/bot"
     LOAD = []
     NO_LOAD = ["rss", "cleaner", "connection", "math"]
     WEBHOOK = False
