763888a75d1359e44ae66cc150ca88da78640235
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2022-06-19 23:30:35 +0700

    Oke

--- EmikoRobot/modules/__init__.py
+++ EmikoRobot/modules/__init__.py
@@ -21,7 +21,7 @@ def __list_all_modules():
                 any(mod == module_name for module_name in all_modules)
                 for mod in to_load
             ):
-                LOGGER.error("[Charlotte] Invalid loadorder names. Quitting.")
+                LOGGER.error("Invalid load order names, Quitting...")
                 sys.exit(1)
 
             all_modules = sorted(set(all_modules) - set(to_load))
@@ -31,7 +31,7 @@ def __list_all_modules():
             to_load = all_modules
 
         if NO_LOAD:
-            LOGGER.info("[Charlotte] Not loading: {}".format(NO_LOAD))
+            LOGGER.info("Not loading: {}".format(NO_LOAD))
             return [item for item in to_load if item not in NO_LOAD]
 
         return to_load
